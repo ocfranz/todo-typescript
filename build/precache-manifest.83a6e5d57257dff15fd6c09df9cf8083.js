9544acb9815eab7d7d2881dfc125651e43c771a8
Franz Ogosi Cahuana <franz.ogosi@usil.pe>
2020-08-24 23:23:37 -0500

    production files

---
+++ build/precache-manifest.83a6e5d57257dff15fd6c09df9cf8083.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c666b899e9c11f6507d062d51ae64c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "06aff10f1ca1413d4694",
+    "url": "/static/css/main.2b8c335b.chunk.css"
+  },
+  {
+    "revision": "f18a63eafd2f3cc14cbf",
+    "url": "/static/js/2.c86fc7fe.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c86fc7fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06aff10f1ca1413d4694",
+    "url": "/static/js/main.4921c0e1.chunk.js"
+  },
+  {
+    "revision": "9194643b99260cc956ec",
+    "url": "/static/js/runtime-main.de43b08f.js"
+  }
+]);
